1e74b482d13c06fe9ba8be4f86cf5f643da1c9fb
Matthias Böhm <mail@matthiasboehm.com>
2019-03-07 19:58:46 +0100

    Sync Docker MySQL db scheme with live db scheme

--- migrations/20190206134449-entries.js
+++ migrations/20190206134449-entries.js
@@ -5,113 +5,136 @@ var type;
 var seed;
 
 /**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
-exports.setup = function(options, seedLink) {
-  dbm = options.dbmigrate;
-  type = dbm.dataType;
-  seed = seedLink;
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+    dbm = options.dbmigrate;
+    type = dbm.dataType;
+    seed = seedLink;
 };
 
-exports.up = function(db, callback) {
-  db.createTable('entries', {
-    id: {
-      type: 'int',
-      unsigned: true,
-      notNull: true,
-      primaryKey: true,
-      autoIncrement: true,
-      length: 11
-    },
-    firstname: {
-      type: 'string',
-      length: 255,
-      notNull: true,
-    },
-    lastname: {
-      type: 'string',
-      length: 255,
-      defaultValue : null,
-    },
-    email: {
-      type: 'string',
-      length: 320,
-      notNull: true,
-    },
-    country: {
-      type: 'char',
-      length: 2,
-      defaultValue : null,
-    },
-    message: {
-      type: 'text',
-      notNull : true,
-    },
-    anon: {
-      type: 'int',
-      length: 1,
-      defaultValue : 0,
-    },
-    ipv4: {
-      type: 'string',
-      length: 40,
-      defaultValue : null,
-    },
-    image: {
-      type: 'string',
-      length: 255,
-      defaultValue : null,
-    },
-    created_at: {
-      type: 'string',
-      length: 15,
-      defaultValue : null,
-    },
-    updated_at: {
-      type: 'string',
-      length: 15,
-      defaultValue : null,
-    },
-    confirm_key: {
-      type: 'string',
-      length: 75,
-      defaultValue : null,
-    },
-    beta: {
-      type: 'int',
-      length: 1,
-      defaultValue : 0,
-    },
-    newsletter: {
-      type: 'int',
-      length: 1,
-      defaultValue : 0,
-    },
-    pax: {
-      type: 'int',
-      length: 1,
-      defaultValue : 0,
-    },
-    email_confirmed: {
-      type: 'int',
-      length: 1,
-      defaultValue : 0,
-    },
-    status: {
-      type: 'int',
-      length: 1,
-      defaultValue : 0,
-    },
-  }, function(err) {
-    if (err) return callback(err);
-    return callback();
-  });
+exports.up = function (db, callback) {
+    db.createTable('entries', {
+        id: {
+            type: 'bigint',
+            unsigned: true,
+            notNull: true,
+            primaryKey: true,
+            autoIncrement: true,
+            unique: true,
+            length: 20
+        },
+        email: {
+            type: 'string',
+            length: 255,
+            notNull: true,
+        },
+        firstname: {
+            type: 'string',
+            length: 255,
+            notNull: true,
+        },
+        lastname: {
+            type: 'string',
+            length: 255,
+            defaultValue: null,
+        },
+        message: {
+            type: 'text',
+            notNull: true,
+        },
+        country: {
+            type: 'string',
+            length: 4,
+            defaultValue: null,
+        },
+        image: {
+            type: 'string',
+            length: 255,
+            notNull: true,
+        },
+        ipv4: {
+            type: 'string',
+            length: 30,
+            defaultValue: null,
+        },
+        ipv6: {
+            type: 'string',
+            length: 60,
+            defaultValue: null,
+        },
+        email_confirmed: {
+            type: 'smallint',
+            length: 1,
+            unsigned: true,
+            notNull: true,
+            defaultValue: '0',
+        },
+        confirm_key: {
+            type: 'string',
+            length: 255,
+            defaultValue: null,
+        },
+        status: {
+            type: 'smallint',
+            length: 1,
+            unsigned: true,
+            notNull: true,
+            defaultValue: '0',
+        },
+        anon: {
+            type: 'smallint',
+            length: 1,
+            unsigned: true,
+            notNull: true,
+            defaultValue: '0',
+        },
+        created_at: {
+            type: 'bigint',
+            length: 20,
+            unsigned: true,
+            defaultValue: null,
+        },
+        updated_at: {
+            type: 'bigint',
+            length: 20,
+            unsigned: true,
+            defaultValue: null,
+        },
+        confirmed_at: {
+            type: 'bigint',
+            length: 20,
+            unsigned: true,
+            defaultValue: null,
+        },
+        beta: {
+            type: 'smallint',
+            length: 1,
+            notNull: true,
+            defaultValue: '0',
+        },
+        newsletter: {
+            type: 'smallint',
+            length: 1,
+            notNull: true,
+            defaultValue: '0',
+        },
+        pax: {
+            type: 'smallint',
+            length: 1,
+            notNull: true,
+            defaultValue: '0',
+        },
+    }, function (err) {
+        if (err) return callback(err);
+        return callback();
+    });
 };
-exports.down = function(db, callback) {
-  db.dropTable('entries', callback);
+exports.down = function (db, callback) {
+    db.dropTable('entries', callback);
 };
 
 exports._meta = {
-  "version": 1
+    'version': 1
 };
