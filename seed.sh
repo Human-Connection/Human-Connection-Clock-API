478bf6a787bd4a1231936dd83666c5785d0f8d69
Matthias Böhm <mail@matthiasboehm.com>
2019-03-08 17:53:46 +0100

    Set database collation to utf8, fix seed entries & readme description

--- seed.sh
+++ seed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mysql --user=$MYSQL_USER --password=$MYSQL_PASSWORD $MYSQL_DATABASE << EOF
-INSERT INTO \`clock_of_change\`.\`apikeys\` (\`secret\`, \`valid\`) VALUES ('secret', '1');
-INSERT INTO \`clock_of_change\`.\`entries\` (\`firstname\`, \`lastname\`, \`email\`, \`country\`, \`message\`, \`confirm_key\`, \`beta\`, \`newsletter\`, \`pax\`, \`email_confirmed\`, \`status\`) VALUES ('Pamela', 'Musterfrau', 'pamela@example.org', 'US', 'Let us change the world!', 'confirm', '1', '0', '1', '1', '1');
-INSERT INTO \`clock_of_change\`.\`entries\` (\`firstname\`, \`lastname\`, \`email\`, \`country\`, \`message\`, \`confirm_key\`, \`beta\`, \`newsletter\`, \`pax\`, \`email_confirmed\`, \`status\`) VALUES ('Max', 'Mustermann', 'max@example.org', 'DE', 'Lasst uns die Welt verbessern!', 'confirm', '0', '1', '1', '1', '1');
+INSERT INTO \`clock_of_change\`.\`apikeys\` (\`secret\`, \`valid\`, \`expire_at\`, \`created_at\`, \`updated_at\`) VALUES ('secret', '1', 1893456001, 1552060594, 1552060594);
+INSERT INTO \`clock_of_change\`.\`entries\` (\`email\`, \`firstname\`, \`lastname\`, \`message\`, \`country\`, \`image\`, \`email_confirmed\`, \`confirm_key\`, \`status\`, \`created_at\`, \`updated_at\`, \`confirmed_at\`, \`newsletter\`, \`pax\`) VALUES ('pamela@example.org', 'Pamela', 'Musterfrau', 'Let us change the world!', 'US', '', '1', 'confirm', '1', '1552060594', '1552060594', '1552060594', '1', '1');
+INSERT INTO \`clock_of_change\`.\`entries\` (\`email\`, \`firstname\`, \`lastname\`, \`message\`, \`country\`, \`image\`, \`email_confirmed\`, \`confirm_key\`, \`status\`, \`created_at\`, \`updated_at\`, \`confirmed_at\`, \`newsletter\`, \`pax\`) VALUES ('max@example.org', 'Max', 'Mustermann', 'Lasst uns die Welt verbessern!', 'DE', '', '1', 'confirm', '1', '1552060594', '1552060594', '1552060594', '0', '1');
 EOF
